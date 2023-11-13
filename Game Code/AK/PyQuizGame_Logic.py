6fa82c46077c4284bd8b4b36dc7c4f9dad6f850a
Aidan Kavanagh <akavanagh003@gmail.com>
2023-11-13 12:10:39 -0700

    Added backend game logic prototype

---
+++ Game Code/AK/PyQuizGame_Logic.py
@@ -0,0 +1,45 @@
+"""
+uri = "mongodb+srv://cluster0.dgvxubp.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+client = MongoClient(uri,
+                     tls=True,
+                     tlsCertificateKeyFile='<path_to_certificate>',
+                     server_api=ServerApi('1'))
+db = client['testDB']
+collection = db['testCol']
+doc_count = collection.count_documents({})
+print(doc_count)
+
+"""
+
+def check_answer(user:str,answer:str) -> bool:
+    if(answer == user):
+        return True
+    else:
+        return False
+    
+def display_score(correct:int,total:int)->None:
+    percent = (correct/total)*100
+    print("Your score is: ", percent, "%\n")
+    print("You got ", correct, " answers right out of ", total," questions")
+
+
+    
+
+questions = {}#array will be loaded with questions
+user = ""
+result = False
+correct_answers = 0
+total_answers = len(questions)
+#game loop
+for i in questions:
+    print(i.question)
+    for j in i.options:
+        print(j)
+    user = int(input("Please enter your answer:"))
+    result = check_answer(i.options[user], i.answer)
+    if(result == True):
+        print("\nCorrect!\n")
+        correct_answers += 1
+    else:
+        print("\nIncorrect\n")
+
